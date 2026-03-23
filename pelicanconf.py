805d4a7386a63a7adeebee06d5dbb579baaed864
Konrad Rieck <rieck@tu-berlin.de>
2026-03-23 15:51:50 +0100

    improved config of poster groups

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,6 +14,7 @@ PROGRAM_COMMITTEE = json.loads(Path('data/pc.json').read_text())
 
 PAST_EDITIONS = json.loads(Path('data/past-editions.json').read_text())
 PAPERS = json.loads(Path('data/papers.json').read_text())
+POSTER_GROUP_SPLIT = [123]  # Thresholds splitting papers into poster groups (e.g. [123, 200] → 3 groups)
 KEYNOTES = json.loads(Path('data/keynotes.json').read_text())
 PROGRAM = json.loads(Path('data/program.json').read_text())
 
